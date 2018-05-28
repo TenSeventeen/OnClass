d12a6827ff6c257c80544834b12d3077ac54e66f
TenSeventeen <517268184@qq.com>
2018-05-28 09:27:10 +0800

    update

--- src/main/java/com/example/demo/ReadyView.java
+++ src/main/java/com/example/demo/ReadyView.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,6 +7,8 @@ import java.util.Scanner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.service.OnClassService;
 
 @Configuration
 public class ReadyView {
